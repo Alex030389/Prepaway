b29bb5b21903c9c01179ae7ad1f35dbf3441de5f
AlexKa <x182838@gmail.com>
2020-04-20 09:51:42 +0300

    page certification

--- src/design/script/main.js
+++ src/design/script/main.js
@@ -15,7 +15,7 @@ const searchBtnBack = document.querySelector('.search__btn-back');
 const searchInput = document.querySelector('.search__input');
 
 window.addEventListener('resize', function() {
-	if(headerTop1 !=null) {
+	if(headerTop1) {
 		if(getComputedStyle(headerSearchForm).display != 'none') {
 			hideMblMenu();
 		}
@@ -27,7 +27,7 @@ window.addEventListener('resize', function() {
 	}
 })
 
-if(headerTop1 !=null) {
+if(headerTop1) {
 	headerMblBtnHamburger.addEventListener('click', function(e) {
 		showMblMenu();
 	});
@@ -89,6 +89,16 @@ $('[data-mfp-src="#modal-4"]').magnificPopup({
 	midClick: true 
 });
 
+$('[data-mfp-src="#modal-5"]').magnificPopup({
+	showCloseBtn: false,
+	midClick: true 
+});
+
+$('[data-mfp-src="#modal-6"]').magnificPopup({
+	showCloseBtn: false,
+	midClick: true 
+});
+
 const modalBtnCloseAr = document.querySelectorAll('.modal__btn-close');
 
 if(modalBtnCloseAr != null) {
@@ -96,8 +106,7 @@ if(modalBtnCloseAr != null) {
 		modalBtnCloseAr[i].addEventListener('click', function() {
 			$.magnificPopup.close();
 		})
-	}
-	
+	}	
 }
 
 
@@ -236,42 +245,6 @@ $('.slick-4').slick({
 });
 
 
-$('.slick-4').slick({
-	dots: true,
-	speed: 300,
-	slidesToShow: 4,
-	slidesToScroll: 4,
-	prevArrow: '<button type="button" class="slick-prev"><svg aria-hidden="true"><use xlink:href="../design/img/symbol/sprite.svg#slider-arrow-prev"></use></svg></button>',
-	nextArrow: '<button type="button" class="slick-next"><svg aria-hidden="true"><use xlink:href="../design/img/symbol/sprite.svg#slider-arrow-next"></use></svg></button>',
-	responsive: [
-		{
-			breakpoint: 1501,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
-			}
-		},
-		{
-			breakpoint: 1200,
-			settings: {
-				arrows: false,
-				slidesToShow: 3,
-				slidesToScroll: 3,
-			}
-		},
-		{
-			breakpoint: 768,
-			settings: {
-				arrows: false,
-				slidesToShow: 2,
-				slidesToScroll: 2,
-			}
-		},
-	]
-});
-
-
-
 $('.slick-5').slick({
 	dots: true,
 	speed: 300,
@@ -304,6 +277,49 @@ $('.slick-5').slick({
 });
 
 
+
+$('.slick-6').slick({
+	infinite: false,
+	dots: true,
+	speed: 300,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	waitForAnimate: false,
+	prevArrow: '<button type="button" class="slick-prev"><svg aria-hidden="true"><use xlink:href="../design/img/symbol/sprite.svg#slider-arrow-prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg aria-hidden="true"><use xlink:href="../design/img/symbol/sprite.svg#slider-arrow-next"></use></svg></button>',
+	responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				arrows: false,
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				arrows: false,
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			}
+		},
+		{
+			breakpoint: 470,
+			settings: {
+				arrows: false,
+				centerMode: false,
+				slidesToShow: 2,
+				slidesToScroll: 2,
+			}
+		},
+	]
+});
+
+
+
+
+// ============================================================== slider-magnific
 $('.slider-4__list').magnificPopup({
 	delegate: 'a',
 	type: 'image',
@@ -311,6 +327,8 @@ $('.slider-4__list').magnificPopup({
 		enabled: true
 	},
 });
+
+
 
 
 
@@ -348,6 +366,80 @@ Array.prototype.forEach.call(inputFile, function (input) {
 	});
 });
 
+
+// =================================================== btn-about-course
+const aboutCourse = document.querySelector('.about-course');
+const btnAboutCourse = document.querySelector('.btn-about-course');
+
+if(btnAboutCourse) {
+	btnAboutCourse.addEventListener('click', function() {
+		aboutCourse.classList.add('_show');
+		btnAboutCourse.classList.add('d-none');
+	})
+}
+
+
+// ==================================================== rating star
+const starForm = document.querySelector('.star-form');
+// const starFormItem = document.querySelectorAll('.star-form__item');
+const starFormLabel = document.querySelectorAll('.star-form__label');
+const starFormInput = document.querySelectorAll('.star-form__input');
+
+
+if(starForm) {
+	for(let i = 0; i < starFormLabel.length; i++) {
+		starFormLabel[i].addEventListener('click', function() {
+			for(let i = 0; i < starFormLabel.length; i++) {
+				starFormLabel[i].classList.remove('_active');
+			}
+
+			for(let ii = 0; ii <= i; ii++) {
+				starFormLabel[ii].classList.add('_active');
+			}
+		})
+
+		starFormLabel[i].addEventListener('mouseenter', function() {
+			for(let i = 0; i < starFormLabel.length; i++) {
+				starFormLabel[i].classList.remove('_hover');
+			}
+
+			for(let ii = 0; ii <= i; ii++) {
+				starFormLabel[ii].classList.add('_hover');
+			}		
+		})
+
+		starFormLabel[i].addEventListener('mouseleave', function() {
+			for(let i = 0; i < starFormLabel.length; i++) {
+				starFormLabel[i].classList.remove('_hover');
+			}
+		})
+	}	
+}
+
+
+// =================================================== video tab
+const slider5List = document.querySelector('.slider-5__list');
+const slider5Item2 = document.querySelectorAll('.slider-5__item-2');
+const tabS = document.querySelector('.tab-s');
+const tabSItems = document.querySelectorAll('.tab-s__item');
+
+for(let i = 0; i < slider5Item2.length; i++) {
+	slider5Item2[i].addEventListener('click', function() {
+		
+		tabS.classList.remove('d-none');
+		
+		for(let i = 0; i < tabSItems.length; i++) {
+			tabSItems[i].classList.add('d-none');
+			tabSItems[i].classList.remove('_fade-in');
+		}
+
+		tabSItems[i].classList.remove('d-none');
+		
+		setTimeout(function() {
+			tabSItems[i].classList.add('_fade-in');
+		},1);
+	})
+}
 
 // todo scrollTop
 // ========================================================= arrow top
